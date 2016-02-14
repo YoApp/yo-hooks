6845a33c117e5675555ef72567c419d9d71b5d74
Or Arbel <orarbel@gmail.com>
2016-02-13 17:59:21 -0800

    Added pingdom.

--- pingdom.py
+++ pingdom.py
@@ -8,10 +8,11 @@ https://help.pingdom.com/hc/en-us/articles/203611322-Setting-up-a-Webhook-and-an
 Installation:
 https://help.pingdom.com/hc/en-us/articles/203611322-Setting-up-a-Webhook-and-an-Alerting-Endpoint
 """
+import json
 
 
 def translate(request):
-	message = request.args.get('message')
-	data = json.loads(message)
+    message = request.args.get('message')
+    data = json.loads(message)
     text = data.get('host') + ' ' + data.get('down')
     return text
