91dddccc512d1ca0cbcc37edcdf8d63d4250fdcc
Or Arbel <orarbel@gmail.com>
2016-02-13 17:57:46 -0800

    Added pingdom.

---
+++ pingdom.py
@@ -0,0 +1,17 @@
+"""
+Description:
+Called on incident created or changed
+
+Documentation:
+https://help.pingdom.com/hc/en-us/articles/203611322-Setting-up-a-Webhook-and-an-Alerting-Endpoint
+
+Installation:
+https://help.pingdom.com/hc/en-us/articles/203611322-Setting-up-a-Webhook-and-an-Alerting-Endpoint
+"""
+
+
+def translate(request):
+	message = request.args.get('message')
+	data = json.loads(message)
+    text = data.get('host') + ' ' + data.get('down')
+    return text
