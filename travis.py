a7e954d8f17d3f76d56880aac5d9c7b66eb922d2
Or Arbel <orarbel@gmail.com>
2016-02-19 16:57:51 -0800

    Added travis.

---
+++ travis.py
@@ -0,0 +1,22 @@
+"""
+Description:
+Called on Travis tests done
+
+Documentation:
+https://docs.travis-ci.com/user/notifications/#Webhook-notification
+
+Installation:
+In circleci.yml add:
+
+    notify:
+      webhooks: https://yohooks.com/travis/<your-yo-username>/
+
+"""
+
+
+def translate(request):
+    payload = request.json
+    subject = payload.get('message')
+    status = payload.get('status_message')
+    text = subject[:25] + u'... ' + status
+    return text
